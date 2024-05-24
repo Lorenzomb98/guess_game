250408067e9035479bee31fabd1f2478d2dca034
Lorenzomb98 <lorenzomora98@gmail.com>
2024-05-24 13:58:05 -0600

    a ver si funca

---
+++ guess_game.py
@@ -0,0 +1,25 @@
+import random
+
+
+def run_guess(guess, answer):
+    try:
+        if 0 < guess < 11:
+            if guess == answer:
+                print('You got it! Congrats genius')
+                return True
+            else:
+                print('Sorry, try again')
+        else:
+            print('Hey bozo, I said 1~10')
+            return False
+    except TypeError as err:
+        print('Please enter a number. Run it again bozo')
+        return err
+
+
+if __name__ == '__main__':
+    answer = random.randint(1, 10)
+    while True:
+        guess = int(input('Guess a number 1~10: '))
+        if (run_guess(guess, answer)):
+            break
